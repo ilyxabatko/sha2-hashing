1d04e7420d84947c3fcf033ec2bb3f99d6748768
ilyxabatko <ilyxabatko@inbox.ru>
2023-10-07 15:26:41 +0400

    improves the perfomance (~as fast as sha256sum)

--- src/lib.rs
+++ src/lib.rs
@@ -2,16 +2,34 @@ use anyhow::Result;
 use sha2::{Sha256, Digest};
 use std::{fs::File, io::Read};
 
+const SIZE: usize = 0x8000;
+
 pub fn hash_file(file_name: &str) -> Result<[u8; 32]> {
     // creates a new hasher instance
     let mut hasher = Sha256::new();
 
     let mut file = File::open(file_name)?;
-    let mut file_data = Vec::new();
+
+    // we'll read the file more than one time (32 KB at most per read call) to improve performance
+    // as the "sha256sum" does accordint to "strace" information
+    let mut file_data = vec![0; SIZE];
+
+    loop {
+        let amount_data_read = file.read(&mut file_data)?;
+
+        if amount_data_read == SIZE {
+            // updates the internal state
+            hasher.update(&file_data);
+        } else {
+            // here we exldue other garbage and passing the exact slice that we want to update 
+            // the hasher with on the last loop iteration
+            hasher.update(&file_data[0..amount_data_read]);
+            break;
+        }
+    }
+
     file.read_to_end(&mut file_data)?;
 
-    // updates the internal state and returns a GenericArray (hash)
-    hasher.update(&file_data);
     let hash = hasher.finalize();
 
     // get a common Rust array that we want to work with
